ef3a5ca5e9b179a06a27d34d56f4c18414e1aa33
Azaan <xxx.azaan.ali@gmail.com>
2022-05-13 03:24:01 -0400

    Winner Outcome Label

--- tictactoe.py
+++ tictactoe.py
@@ -1,5 +1,6 @@
-from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QLabel, QGridLayout, QWidget
-from PyQt5.QtCore import QSize
+from PyQt5.QtWidgets import QPushButton, QLabel, QGridLayout, QWidget
+from PyQt5.QtCore import QSize, Qt
+from PyQt5.QtGui import QFont
 import random
 
 class TicTacToe(QWidget):
@@ -12,8 +13,14 @@ class TicTacToe(QWidget):
         for i in range(3): 
            for j in range(3): 
                 self.button_list[(i, j)] = QPushButton()
+                self.button_list[(i, j)].setMinimumHeight(100)
+                self.button_list[(i, j)].setFont(QFont("Times", 50, QFont.Bold))
                 self.button_list[(i, j)].clicked.connect(self.onClick)
                 self.layout.addWidget(self.button_list[(i, j)], i, j)
+        self.label = QLabel('Your Turn', self)
+        self.label.setAlignment(Qt.AlignCenter)
+        self.layout.addWidget(self.label, 4, 0)
+
         self.setLayout(self.layout)
         self.turns = 0
 
@@ -24,6 +31,7 @@ class TicTacToe(QWidget):
         button.setEnabled(False)
         if not self.check_winner():
             if self.turns == 9:
+                self.label.setText("Draw")
                 return
             self.turns += 1
             self.cpu()
@@ -43,34 +51,34 @@ class TicTacToe(QWidget):
         for i in range(3):
             if self.button_list[(0, i)].text() == self.button_list[(1, i)].text() and self.button_list[(0, i)].text() == self.button_list[(2, i)].text() and self.button_list[(0, i)].text() != "":
                 if self.button_list[(0, i)].text() == "X":
-                    pass
+                    self.label.setText("You Lost")
                 else:
-                    pass
+                    self.label.setText("You Won")
                 self.disableButtons()
                 return True
 
         for i in range(3):
             if self.button_list[(i, 0)].text() == self.button_list[(i, 1)].text() and self.button_list[(i, 0)].text() == self.button_list[(i, 2)].text() and self.button_list[(i, 0)].text() != "":
                 if self.button_list[(i, 0)].text() == "X":
-                    pass 
+                    self.label.setText("You Lost") 
                 else:
-                    pass
+                    self.label.setText("You Won")
                 self.disableButtons()
                 return True
 
         if self.button_list[(0, 0)].text() == self.button_list[(1, 1)].text() and self.button_list[(0, 0)].text() == self.button_list[(2, 2)].text() and self.button_list[(0, 0)].text() != "":
             if self.button_list[(0, 0)].text() == "X":
-                pass 
+                self.label.setText("You Lost")
             else:
-                pass
+                self.label.setText("You Won")
             self.disableButtons()
             return True
 
         if self.button_list[(0, 2)].text() == self.button_list[(1, 1)].text() and self.button_list[(0, 2)].text() == self.button_list[(2, 0)].text() and self.button_list[(0, 2)].text() != "":
             if self.button_list[(0, 2)].text() == "X":
-                pass 
+                self.label.setText("You Lost") 
             else:
-                pass
+                self.label.setText("You Won")
             self.disableButtons()
             return True
 
